9bd4f118ff875a3d60e809a067efed45e60ed39b
Ziga Strgar <ziga_strgar@hotmail.com>
2016-09-28 18:58:58 +0200

    Updated tester

--- tester.php
+++ tester.php
@@ -1,15 +1,23 @@
 <?php
 
 require_once 'Searcher.php';
-$init1 = time();
 
-$search = new Searcher("3 sobno stanovanje v ljubljani od 640 - 660€ Bežigrad do 80 kvadratov");
+$init1  = time();
+$query  =
+    ( isset( $_GET['query'] ) ) ? $_GET['query'] : "oddaja 3 sobno stanovanje v ljubljani mjejsto od 640 - 660€ Bežigrad od 70 - 80 kvadratov";
+$search = new Searcher($query);
 
 echo "<pre>";
-echo $search->getSql()."<br />";
-print_r($search->getResults());
+echo $search->getSql() . "<br />";
+print_r($search->getResults(true));
 echo "</pre>";
 
 $total = time() - $init1;
 
 echo "$total ms";
+
+?>
+<form action="" method="GET">
+    <input type="text" name="query" width="500">
+    <input type="submit">
+</form>
