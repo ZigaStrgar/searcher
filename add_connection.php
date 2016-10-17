e83911f4790f835beeaf31a22aa0c8e12891cedb
Ziga Strgar <ziga_strgar@hotmail.com>
2016-10-17 12:50:27 +0200

    Renamed to index and searchify included in adding

--- add_connection.php
+++ add_connection.php
@@ -2,6 +2,14 @@
 
 require 'Str.php';
 
+function searchify($string)
+{
+    $ascii = Str::ascii($string);
+    $str   = new Str();
+
+    return $str->lower($ascii);
+}
+
 $connection = mysqli_connect('localhost', 'root', '', 'searcher');
 mysqli_query($connection, "SET NAMES UTF8;");
 
@@ -16,15 +24,16 @@ if ( $_POST['type'] == 'existing' ) {
     $query  =
         "UPDATE cr_keywords SET cr_table = '{$idData['cr_table']}', cr_id = {$idData['cr_id']}, `column` = $column WHERE id = " . $typo;
     if ( mysqli_query($connection, $query) ) {
-        header('Location: connector.php');
+        header('Location: index.php');
     } else {
         echo "Napaka pri povezovanju";
     }
 } else {
     $column = ( strlen($idData['column']) > 0 ) ? "'" . $idData['column'] . "'" : 'NULL';
+    $typo   = searchify($typo);
     $typo   = ( strpos($typo, " ") !== false || strlen($typo) < 3 ) ? '"' . $typo . '"' : $typo;
     if ( mysqli_query($connection, "INSERT INTO cr_keywords (text, cr_id, cr_table, `column`) VALUES ('{$typo}', {$idData['cr_id']}, '{$idData['cr_table']}', $column);") ) {
-        header('Location: connector.php');
+        header('Location: index.php');
     } else {
         echo "Napaka pri ustvarjanju nove povezave";
     }
