3864ebba62c12cb15a70f0ea21bbff8f003a16af
Ziga Strgar <ziga_strgar@hotmail.com>
2016-10-24 12:34:40 +0200

    Added files back

--- header.php
+++ header.php
@@ -1,36 +1,33 @@
-<?php include_once 'database.php'; ?>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-              crossorigin="anonymous">
-        <link rel="stylesheet" href="css/select2.min.css">
-        <title>Searcher configuration</title>
-    </head>
-    <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Searcher configurator</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Dodajanje in kreiranje povezav</a></li>
-                    <li><a href="status.php">Deaktivacija in aktivacija besed</a></li>
-                    <li><a href="logs.php">Pregled iskanj</a></li>
-                </ul>
-            </div>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/select2.min.css">
+    <title>Searcher configuration</title>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Searcher configurator</a>
         </div>
-    </nav>
-    <div class="container">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Dodajanje in kreiranje povezav</a></li>
+                <li><a href="status.php">Deaktivacija in aktivacija besed</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
 <?php include_once 'notification.php'; ?>
---
+++ tester.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'Searcher.php';
+
+$init1  = time();
+$query  =
+    ( isset( $_GET['query'] ) ) ? $_GET['query'] : "toplarna oddaja 3 sobno stanovanje v ljubljani mjejsto od 640 - 660€ Bežigrad balkon";
+$search = new Searcher($query);
+?>
+    <form action="" method="GET">
+        <input type="text" name="query" value="<?php echo $_GET['query'] ?>" width="500">
+        <input type="submit">
+    </form>
+<?php
+
+echo "<pre>";
+echo $search->getSql() . "<br />";
+print_r($search->getResults(true));
+echo "</pre>";
+
+$total = time() - $init1;
+
+echo "$total ms";
+
+?>
